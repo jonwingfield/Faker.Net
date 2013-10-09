954403da56568124b12740311915ebf9c9bcca62
PulsarBlow <pulsarblow@gmail.com>
2013-10-09 20:21:18 +0200

    NuGet Packaging
    Signed-off-by: PulsarBlow <pulsarblow@gmail.com>

--- Faker.Net/Properties/AssemblyInfo.cs
+++ Faker.Net/Properties/AssemblyInfo.cs
@@ -1,35 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Faker.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FFaker.Net")]
+[assembly: AssemblyDescription("As much as possible, a line-for-line port of ffaker for ruby (https://github.com/EmmanuelOga/ffaker). Supports .NET Framework and Mono.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Faker.Net")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Jon Wingfield")]
+[assembly: AssemblyProduct("FFaker.Net")]
+[assembly: AssemblyCopyright("Copyright (c) 2011, Jon Wingfield (wingfield.jon@gmail.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c1626824-b4d0-42ed-a767-02bf6a146c24")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
--- README.markdown
+++ README.markdown
@@ -1,41 +1,12 @@
-Faker.NET
-================================================
+# FFaker.Net
 
-As much as possible, a line-for-line port of ffaker for ruby (https://github.com/EmmanuelOga/ffaker).
+#### A simple NuGet Package wrapper for Faker.Net
 
-Relies on ideas taken from Rob Conery's and Jon Wingfield's excellent sugar library (https://github.com/jonwingfield/sugar). I have just taken the idea to minimize the assembly's dependencies.
+This is the project built upon Faker.Net by Jon Wingfield (https://github.com/jonwingfield/Faker.Net)
 
-Supports .NET Framework and Mono.
+It serves as the base project for the FFaker.Net NuGet Package.
 
+For documentation and examples, check : https://github.com/jonwingfield/Faker.Net
 
-Building
-------------------------------------------------
-
-nant build
-
-nant build -D:mode=Debug
-
-
-Tests
-------------------------------------------------
-
-There is a project in the solution named Faker.Net.NUnit.Tests that uses the relevant ffaker tests written in Ruby to test the .NET code.
-
-NAnt command to run unit-tests
-
-nant test
-
-nant test -D:mode=Debug
-
-
-Cleaning build output
--------------------------------------------------
-
-nant clean
-
-nant clean -D:mode=Debug
-
-
-Contrib
-------------------------------------------------
-This is my first open source project, so any and all feedback/pull requests are welcomed and requested.
+##### Why FFaker and not Faker ?
+Simply because the name Faker.Net wasn't available on NuGet.
