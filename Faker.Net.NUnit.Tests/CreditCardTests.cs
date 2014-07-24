f3302a72752f2943bc26cae5b15f9dd5de84b2cf
Jorge Avelar <jorge.avelar@locaweb.com.br>
2014-07-24 17:02:09 -0300

    Added Gen credit card class

---
+++ Faker.Net.NUnit.Tests/CreditCardTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System;
+
+namespace Faker.Tests
+{
+	[TestFixture()]
+	public class CreditCardTests
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+		[Test()]
+		public void TestNumber()
+		{
+
+
+		}
+
+	}
+}
+
---
+++ Faker.Net/CreditCard.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Faker
+{
+	public class CreditCard
+	{
+		public CreditCard ()
+		{
+		}
+
+
+		public enum CreditCardType
+		{
+			Visa,
+			MasterCard,
+			DinnersClub
+		}
+
+
+		public string CreateCreditCardNumber(string Type){
+			return null;
+		}
+
+
+	}
+}
+
